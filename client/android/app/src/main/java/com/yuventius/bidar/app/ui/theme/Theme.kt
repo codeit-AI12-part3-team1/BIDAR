c20c542493bb0d8c7ce3c543633ba2cf60e7ae23
Ven_Choi <yuventius125@gmail.com>
2026-09-01 16:23:10 +0900

    fix: Light Theme 고정

--- client/android/app/src/main/java/com/yuventius/bidar/app/ui/theme/Theme.kt
+++ client/android/app/src/main/java/com/yuventius/bidar/app/ui/theme/Theme.kt
@@ -34,20 +34,21 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun BIDARTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+//    val colorScheme = when {
+//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+//            val context = LocalContext.current
+//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+//        }
+//
+//        darkTheme -> DarkColorScheme
+//        else -> LightColorScheme
+//    }
+    val colorScheme = LightColorScheme
 
     MaterialTheme(
         colorScheme = colorScheme,
