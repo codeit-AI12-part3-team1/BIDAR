79f40ddbd315462ad1213827bf5c86b8d1d5d278
Ven_Choi <yuventius125@gmail.com>
2026-09-05 00:58:45 +0900

    feat: 컴포넌트 UI 업데이트

--- client/android/app/src/main/java/com/yuventius/bidar/app/ui/view/common/component/ChatSenderView.kt
+++ client/android/app/src/main/java/com/yuventius/bidar/app/ui/view/common/component/ChatSenderView.kt
@@ -1,7 +1,22 @@
 package com.yuventius.bidar.app.ui.view.common.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.yuventius.bidar.app.ui.theme.MidnightIndigo
+import com.yuventius.bidar.app.ui.theme.MidnightIndigo60
+import com.yuventius.bidar.app.ui.theme.White
+import com.yuventius.bidar.app.util.DatePattern
+import com.yuventius.bidar.app.util.formatByDatePattern
+import com.yuventius.bidar.domain.model.Chat
 import java.time.LocalDateTime
 
 /**
@@ -12,8 +27,29 @@ import java.time.LocalDateTime
 @Composable
 fun ChatSenderView (
     modifier: Modifier = Modifier,
-    msg: String = "TEST MSG",
-    localDateTIme: LocalDateTime = LocalDateTime.now()
+    chat: Chat = Chat()
 ) {
+    Column (
+        modifier = modifier,
+        horizontalAlignment = Alignment.End
+    ) {
+        Box (
+            modifier = Modifier
+                .background(MidnightIndigo, shape = RoundedCornerShape(10.dp))
+        ) {
+            Text (
+                modifier = Modifier
+                    .padding(8.dp),
+                text = chat.msg,
+                color = White,
+                fontSize = 12.sp
+            )
+        }
 
+        Text (
+            text = chat.chatDate.formatByDatePattern(DatePattern.CHAT_TIME),
+            fontSize = 10.sp,
+            color = MidnightIndigo60
+        )
+    }
 }
--- client/android/app/src/main/java/com/yuventius/bidar/app/ui/view/common/component/ConfigCardView.kt
+++ client/android/app/src/main/java/com/yuventius/bidar/app/ui/view/common/component/ConfigCardView.kt
@@ -1,7 +1,24 @@
 package com.yuventius.bidar.app.ui.view.common.component
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.yuventius.bidar.R
+import com.yuventius.bidar.app.ui.theme.White
+import com.yuventius.bidar.app.util.DatePattern
+import com.yuventius.bidar.app.util.formatByDatePattern
+import com.yuventius.bidar.app.util.noRippleClickable
 
 /**
  * BIDAR
@@ -10,7 +27,24 @@ import androidx.compose.ui.Modifier
  */
 @Composable
 fun ConfigCardView (
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    configTitle: String,
+    onClick: () -> Unit = {}
 ) {
+    Row (
+        modifier = modifier
+            .background(White, shape = RoundedCornerShape(10.dp))
+            .padding(8.dp)
+            .noRippleClickable(onClick = onClick),
+        horizontalArrangement = Arrangement.spacedBy(15.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(configTitle)
+        Spacer(Modifier.weight(1F))
+        Image (
+            painter = painterResource(R.drawable.ic_chevron_right),
+            contentDescription = null
+        )
+    }
 
 }
