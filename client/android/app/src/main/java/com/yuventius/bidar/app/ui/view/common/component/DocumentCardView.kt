d770fc4ac434e8e417432f89e2e7c4014d0f0895
Ven_Choi <yuventius125@gmail.com>
2026-09-07 13:25:37 +0900

    feat: Home 화면 UI 업데이트 fix: 앱테마 색상 수정 fix: 서버 주소 변경 feat: 리소스 추가

--- client/android/app/src/main/java/com/yuventius/bidar/app/ui/view/common/component/DocumentCardView.kt
+++ client/android/app/src/main/java/com/yuventius/bidar/app/ui/view/common/component/DocumentCardView.kt
@@ -6,15 +6,21 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.yuventius.bidar.R
+import com.yuventius.bidar.app.ui.theme.MidnightIndigo
 import com.yuventius.bidar.app.ui.theme.White
 import com.yuventius.bidar.app.util.DatePattern
 import com.yuventius.bidar.app.util.formatByDatePattern
@@ -34,25 +40,35 @@ fun DocumentCardView (
 ) {
     Row (
         modifier = modifier
-            .background(White, shape = RoundedCornerShape(10.dp))
-            .padding(horizontal = 8.dp)
+            .background(Color.White, shape = RoundedCornerShape(10.dp))
+            .padding(8.dp)
             .noRippleClickable(onClick = onClick),
         horizontalArrangement = Arrangement.spacedBy(15.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Image (
+            modifier = Modifier
+                .size(40.dp),
             painter = painterResource(R.drawable.ic_document),
-            contentDescription = null
+            contentDescription = null,
+            colorFilter = ColorFilter.tint(MidnightIndigo)
         )
         Column (
+            modifier = Modifier
+                .weight(1F),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             Text(document.title)
             val dateString = document.lastChatDate?.formatByDatePattern(DatePattern.CHAT_HISTORY) ?: "없음"
-            Text("최종채팅일자: $dateString")
+            Text (
+                "최종채팅일자: $dateString",
+                fontSize = 12.sp,
+                color = Color.LightGray
+            )
         }
-        Spacer(Modifier.weight(1F))
         Image (
+            modifier = Modifier
+                .size(24.dp),
             painter = painterResource(R.drawable.ic_chevron_right),
             contentDescription = null
         )
